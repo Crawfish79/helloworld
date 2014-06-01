4f4925dacedb0365306823f92798e06c471ee358
Crawfish79 <rocraw79@gmail.com>
2014-06-01 03:57:01 -0400

    hello.php file

---
+++ hello.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Hello in PHP</title>
+<meta charset="utf-8">
+</head>
+ <body>
+  <h1>Hello in PHP</h1>
+  <?php
+   print "Hello, World!";
+   phpInfo();
+  ?>
+ 
+ </body>
+</html> 
